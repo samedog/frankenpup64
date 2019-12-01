7c6029dca1c48bcc57bb015296ca769157ed809d
samedog <the.samedog@gmail.com>
2019-11-30 23:41:14 -0300

    Linux kernel movd to own branch

--- linux-kernel-5.3.11-aufs/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Nov 16 11:47:53 -03 2019"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "puppypc18637"
-#define LINUX_COMPILER "gcc version 5.5.0 (GCC)"
